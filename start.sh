1c37a8d6cc8a8cd655b628e9c20505adb37489c6
wellyerfyanto-arch <wellyerfyanto@gmail.com>
2025-11-08 16:30:02 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 echo "=== Starting Traffic Automation ==="
-echo "PORT: $PORT"
+echo "Using PORT: $PORT"
 
-# Use Python module to run gunicorn
 python -m gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 2 --timeout 120 app:app
